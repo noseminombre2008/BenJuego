74ad57503ed06692138cfa0dac6523a030cb1bcc
Santiago Martinez <peladio98@gmail.com>
2024-06-01 10:31:04 -0400

    primer intento

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener"></a> 
+      <a href="https://vuejs.org/" target="_blank" rel="noopener"></a>
+    </h3>
+  </div>
+</template>
+
+
